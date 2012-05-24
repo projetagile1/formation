d358b2cc8a477fba6af5d1b792381f75a18029f9
Pierre Merlin <pierrem@apido.org>
2012-05-25 00:19:57 +0200

    Happy and agile first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Formation::Application.config.secret_token = '31328421c19858faebcf1d2968ed3671cd23ad256aeb9515447ad13a3436a38d4197300f3e6b9f130feeb64fd0aae1b6e1393fb1afcc828b841525f3e75d79d2'
